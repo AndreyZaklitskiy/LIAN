f216dcea863e8fd404a6336edc6e8daa927141bd
AndreyZaklitskiy <glockman94@gmail.com>
2019-03-27 20:20:03 +0200

    my commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   let hml = $('.header-menu').find('.header-menu-item a');
   $('.header-menu-item a').on('click', function () {
-    $(this).closest('.header-menu-item a').removeClass('active');
+    $(this).removeClass('active');
     $(this).addClass('active');
   });
 });
