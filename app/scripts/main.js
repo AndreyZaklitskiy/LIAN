b1b54aa3a355e05132dd23548afe405e61b60bf0
AndreyZaklitskiy <glockman94@gmail.com>
2019-04-02 21:21:20 +0300

    adaptive top work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
   let hml = $('.header-menu').find('.header-menu-item a');
-  $('.header-menu-item a').on('click', function () {
-    $(this).removeClass('active');
+  $('.header-menu-item').on('click', function () {
+    $(this).siblings().removeClass('active');
     $(this).addClass('active');
   });
+
+  //slick main carousel
+
+  $('.home-main-slider').slick({
+    dots: true,
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+  });
+
 });
+
+
+
