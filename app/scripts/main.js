9f66e26b723717e25a466b4cafec2727286f6019
AndreyZaklitskiy <glockman94@gmail.com>
2019-04-30 20:18:52 +0300

    home

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,12 +24,15 @@ $(document).ready(function () {
     /*prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',*/
 
-  $('.waterfall-block').masonry({
+  $('.waterfall').masonry({
     // options
     itemSelector: '.waterfall-item',
     columnWidth: 360,
     gutter: 15
   });
+
+  $('input[type="range"]').rangeslider();
+
 });
 
 
